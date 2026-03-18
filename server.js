3f99b13650566cfcbd0d57df1b088ea16cad6412
Ivanlemak <ivanlemak514@gmail.com>
2026-03-18 16:07:50 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -143,6 +143,8 @@ app.get('/', (req, res) => {
 </html>
     `);
 });
+app.use(express.static(path.join(__dirname)));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.listen(PORT, () => {
     console.log(\`Сервер працює на порту \${PORT}\`);
